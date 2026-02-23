b05467a64900bb43291d41cc20e018fce6c32a8a
Aniqa Shehzadi <aniqamukhtar1@gmail.com>
2026-02-22 21:01:01 -0800

    Initial commit

---
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.demoapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val buttonDark= findViewById<Button>(R.id.btnDark)
+        val buttonLight= findViewById<Button>(R.id.btnLight)
+        val layout= findViewById<LinearLayout>(R.id.linearLayout)
+
+        buttonLight.setOnClickListener{
+            layout.setBackgroundColor(getColor(R.color.yellow))
+
+            
+        }
+        buttonDark.setOnClickListener{
+            layout.setBackgroundColor(getColor(R.color.black))
+
+
+        }
+
+    }
+}
